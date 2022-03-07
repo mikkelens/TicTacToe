1a6598a5144f54e3cf51b3d54f82d2ba9740ea1b
Mikkel S <Mikkel123sej@gmail.com>
2022-03-07 19:33:35 +0100

    Moved files (no more dumb category) and moved Shaders directoy into Assets

---
+++ Assets/Shaders/Pixelation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+[ExecuteInEditMode]
+public sealed class Pixelation : MonoBehaviour
+{
+    public Material Mat;
+    [Space]
+    public uint PixelDesity = 100;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Mat.SetFloat("_AspektRatio", (float)Screen.width / Screen.height);
+        Mat.SetInt("_PixelDensity", (int)PixelDesity);
+
+        Graphics.Blit(source, destination, Mat);
+    }
+}
