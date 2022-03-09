7964f971e5a43fd87737bcfaf24628091a4ce73c
Aether6969 <97663161+Aether6969@users.noreply.github.com>
2022-03-09 13:55:17 +0100

    Eathbound looking ass

--- Assets/Scripts/BGAnimator.cs
+++ Assets/Scripts/BGAnimator.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class BGAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Material BGM;
+
+    public Vector2 Direction;
+    public float Speed;
+
+    private void Awake()
     {
-        
+        BGM = this.GetComponent<MeshRenderer>().material;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnRenderObject()
     {
-        
+        BGM.SetTextureOffset("_MainTex", Direction * Speed * Time.timeSinceLevelLoad);
     }
 }
