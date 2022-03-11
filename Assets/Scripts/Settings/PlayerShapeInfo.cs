d9c818df717ffa3174f34d5713797987f471f2f7
Aether6969 <97663161+Aether6969@users.noreply.github.com>
2022-03-11 11:20:52 +0100

    bruh

--- Assets/Scripts/Settings/PlayerShapeInfo.cs
+++ Assets/Scripts/Settings/PlayerShapeInfo.cs
@@ -7,9 +7,9 @@ public class PlayerShapeInfo : ScriptableObject
     [Required] public PlayerType type;
     [Required] public GameObject prefab;
     [Required] public Color normalColor;
-    [Required] public Color normalEmission;
+    [Required] [ColorUsage(false, true)] public Color normalEmission;
     [Required] public Color permColor;
-    [Required] public Color permEmission;
+    [Required] [ColorUsage(false, true)] public Color permEmission;
     [Required] public Color permOutline;
     [Required] public Sprite icon;
     [Required] public AudioClip landSfx;
